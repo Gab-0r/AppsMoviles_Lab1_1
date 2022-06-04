d1ff9903208c3ec33773740685fb586d44dc93ff
Gabriel <j_gabrielorozco@live.com>
2022-06-04 11:07:34 -0500

    Se implementa satisfactoriamente el date picker. Faltante: Spinner y boton de registro con despliegue de info + verificacion.

--- app/src/main/java/com/example/appsmoviles_lab1_1/MainActivity.kt
+++ app/src/main/java/com/example/appsmoviles_lab1_1/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.appsmoviles_lab1_1
 
+import android.app.Activity
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.appsmoviles_lab1_1.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+    private var borndate = ""
+    private val calendar  = Calendar.getInstance()
+    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+        setContentView(view)
+        
+        val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val format = "dd/MM/yyyy"
+            val sdf = SimpleDateFormat(format)
+            borndate = sdf.format(calendar.time).toString()
+            mainBinding.borndDateButton.text = borndate
+        }
+
+        with(mainBinding){
+            borndDateButton.setOnClickListener{
+                DatePickerDialog(
+                    this@MainActivity,
+                    dateSetListener,
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+        }
     }
 }
